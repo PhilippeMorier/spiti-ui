95fd354f180c4645622f5ba9834f42fb928f0d41
PhilippeMorier <morier.dev@outlook.com>
2017-03-16 20:50:51 +0000

    feat(angular): New generated project with `angular-cli`

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { SpitiUiPage } from './app.po';
+
+describe('spiti-ui App', () => {
+  let page: SpitiUiPage;
+
+  beforeEach(() => {
+    page = new SpitiUiPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('spt works!');
+  });
+});
---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'spt-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'spt works!';
+}
