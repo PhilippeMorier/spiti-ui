79e172525ffcb58324504c4bfff343aaab410d17
PhilippeMorier <morier.dev@outlook.com>
2017-04-01 19:17:21 +0100

    chore(karma): run tests in firefox as well

--- karma.conf.js
+++ karma.conf.js
@@ -8,6 +8,7 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
@@ -39,7 +40,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     // https://github.com/karma-runner/karma-chrome-launcher/issues/83#issuecomment-280423282
-    browsers: ['HeadlessChrome'],
+    browsers: ['HeadlessChrome', 'Firefox'],
     customLaunchers: {
       HeadlessChrome: {
         base: 'Chrome',
