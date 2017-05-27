1af0970dd8bc464006ac1cc8477b14cca0df64db
PhilippeMorier <morier.dev@outlook.com>
2017-05-27 16:52:41 +0100

    feat(fullstory): integrate fullstory in production environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,8 +12,10 @@ import {
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Fullstory } from './fullstory';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
@@ -38,4 +40,9 @@ import { LoginComponent } from './login/login.component';
   providers: [],
 })
 export class AppModule {
+  public constructor() {
+    if (environment.production) {
+      Fullstory.init();
+    }
+  }
 }
