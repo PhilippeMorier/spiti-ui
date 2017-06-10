301bff63b645fa8f6f48f0802111b98e76d7ac95
PhilippeMorier <morier.dev@outlook.com>
2017-06-10 19:08:43 +0100

    build(environment): use `development` & `production` as environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyDaM1qsd-H3TOfknWSGi2yGsqvl5UkyMUA',
+    authDomain: 'spiti-development.firebaseapp.com',
+    databaseURL: 'https://spiti-development.firebaseio.com',
+    messagingSenderId: '348703128194',
+    projectId: 'spiti-development',
+    storageBucket: 'spiti-development.appspot.com',
+  },
+  name: 'DEVELOPMENT',
+  production: false,
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,10 @@
 // The file contents for the current environment will overwrite these during build.
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// `ng build --env=production` then `environment.production.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  firebase: {
-    apiKey: 'AIzaSyA0CoFOWImFtxtiNcIQUwHfsthFc-U1-XI',
-    authDomain: 'spiti-develop.firebaseapp.com',
-    databaseURL: 'https://spiti-develop.firebaseio.com',
-    messagingSenderId: '998869507129',
-    projectId: 'spiti-develop',
-    storageBucket: 'spiti-develop.appspot.com',
-  },
-  name: 'LOCAL',
+  firebase: {},
+  name: '',
   production: false,
 };
