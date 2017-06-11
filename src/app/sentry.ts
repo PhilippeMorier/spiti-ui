943c5f2b044303e0495d31a6efdeb80be27fc0a7
PhilippeMorier <morier.dev@outlook.com>
2017-06-11 18:05:18 +0100

    feat(sentry): identify user after
    * clear context on sign-out

--- src/app/sentry.ts
+++ src/app/sentry.ts
@@ -2,13 +2,32 @@ import { ErrorHandler, Inject, Injectable, InjectionToken } from '@angular/core'
 import * as ravenJs from 'raven-js';
 
 import { environment } from '../environments/environment-source';
+import { SessionService, User } from './session.service';
 
 export const RAVEN = new InjectionToken('Raven');
 
 @Injectable()
 export class Sentry extends ErrorHandler {
-  public constructor(@Inject(RAVEN) private readonly raven: typeof ravenJs) {
+  public constructor(
+    @Inject(RAVEN) private readonly raven: typeof ravenJs,
+    private readonly session: SessionService,
+  ) {
     super();
+
+    this.session
+      .currentlySignedInUser
+      .subscribe((user: User) => {
+          if (user) {
+            this.raven.setUserContext({
+              email: user.email,
+              id: user.uid,
+              username: user.displayName,
+            });
+          } else {
+            this.raven.setUserContext();
+          }
+        },
+      );
   }
 
   public init(): void {
