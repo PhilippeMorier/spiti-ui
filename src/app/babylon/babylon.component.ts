f49a321c2c6fec0baf062a8b59b6ac7556fc9311
PhilippeMorier <morier.dev@outlook.com>
2017-07-09 15:50:21 +0100

    refactor(babylon): add basic babylon component

---
+++ src/app/babylon/babylon.component.ts
@@ -0,0 +1,56 @@
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import * as BABYLON from 'babylonjs';
+
+@Component({
+  selector: 'spt-babylon',
+  styleUrls: [ './babylon.component.scss' ],
+  templateUrl: './babylon.component.html',
+})
+export class BabylonComponent implements AfterViewInit {
+
+  @ViewChild('canvas') private canvas: ElementRef;
+
+  private engine: BABYLON.Engine;
+  private scene: BABYLON.Scene;
+  private camera: BABYLON.FreeCamera;
+  private light: BABYLON.Light;
+
+  public ngAfterViewInit(): void {
+    this.engine = new BABYLON.Engine(this.canvas.nativeElement, true);
+
+    this.createScene();
+    this.animate();
+  }
+
+  public createScene(): void {
+    this.scene = new BABYLON.Scene(this.engine);
+    this.camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), this.scene);
+
+    this.camera.setTarget(BABYLON.Vector3.Zero());
+    this.camera.attachControl(this.canvas.nativeElement, false);
+
+    this.light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), this.scene);
+
+    const sphere = BABYLON.MeshBuilder.CreateSphere('sphere1', {
+      segments: 16,
+      diameter: 2,
+    }, this.scene);
+    sphere.position.y = 1;
+
+    BABYLON.MeshBuilder.CreateGround('ground1', {
+      width: 6,
+      height: 6,
+      subdivisions: 2,
+    }, this.scene);
+  }
+
+  private animate(): void {
+    this.engine.runRenderLoop(() => {
+      this.scene.render();
+    });
+
+    window.addEventListener('resize', () => {
+      this.engine.resize();
+    });
+  }
+}
--- src/app/editor/editor.module.ts
+++ src/app/editor/editor.module.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { BabylonComponent } from '../babylon/babylon.component';
 import { EditorRoutingModule } from './editor-routing.module';
 import { EditorComponent } from './editor.component';
 
 @NgModule({
   declarations: [
     EditorComponent,
+    BabylonComponent,
   ],
   imports: [
     CommonModule,
