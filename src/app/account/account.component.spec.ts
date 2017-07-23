9e7cc635296a7ad977cda16a024e966af36810e2
PhilippeMorier <morier.dev@outlook.com>
2017-07-23 17:01:57 +0100

    test(account): fix specs by injecting `SessionService` & `MdCardModule`

--- src/app/account/account.component.spec.ts
+++ src/app/account/account.component.spec.ts
@@ -1,16 +1,39 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MdCardModule } from '@angular/material';
+import { Observable } from 'rxjs/Observable';
 
+import { SessionService, User } from '../session.service';
 import { AccountComponent } from './account.component';
 
 describe('AccountComponent', () => {
   let component: AccountComponent;
   let fixture: ComponentFixture<AccountComponent>;
+  let mockSessionService: SessionService;
+  let mockUser: User;
+
+  beforeEach(() => {
+    mockUser = {
+      displayName: 'Philippe',
+      email: 'philippe@test.com',
+      uid: '4242',
+    };
+    mockSessionService = jasmine.createSpyObj<SessionService>(
+      'mockSessionService',
+      [ 'currentlySignedInUser' ],
+    );
+    (<jasmine.Spy> mockSessionService.currentlySignedInUser)
+      .and.returnValue(Observable.of(mockUser));
+  });
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AccountComponent ],
-    })
-    .compileComponents();
+        declarations: [ AccountComponent ],
+        imports: [ MdCardModule ],
+        providers: [
+          { provide: SessionService, useValue: mockSessionService },
+        ],
+      })
+      .compileComponents();
   }));
 
   beforeEach(() => {
