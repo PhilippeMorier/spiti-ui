b30966e258e3f60eb72623ea901ae13cb665c337
PhilippeMorier <morier.dev@outlook.com>
2017-07-23 16:36:38 +0100

    test(editor): fix specs by injecting mock `AngularFireDatabase`

--- src/app/editor/editor.component.spec.ts
+++ src/app/editor/editor.component.spec.ts
@@ -1,4 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 import { BabylonComponent } from '../babylon/babylon.component';
 import { EditorComponent } from './editor.component';
@@ -6,12 +9,26 @@ import { EditorComponent } from './editor.component';
 describe('EditorComponent', () => {
   let component: EditorComponent;
   let fixture: ComponentFixture<EditorComponent>;
+  let mockDb: AngularFireDatabase;
+
+  beforeEach(() => {
+    mockDb = jasmine.createSpyObj('db', [ 'list' ]);
+    (<jasmine.Spy> mockDb.list)
+      .and.returnValue(Observable.of([
+      {
+        name: 'subscription 1',
+      },
+    ]));
+  });
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
         declarations: [
           BabylonComponent,
           EditorComponent,
+        ],
+        providers: [
+          { provide: AngularFireDatabase, useValue: mockDb },
         ],
       })
       .compileComponents();
@@ -24,6 +41,7 @@ describe('EditorComponent', () => {
   });
 
   it('should be created', () => {
-    expect(component).toBeTruthy();
+    const firstListItem = fixture.debugElement.query(By.css('li'));
+    expect(firstListItem.nativeElement.innerText).toEqual('subscription 1');
   });
 });
