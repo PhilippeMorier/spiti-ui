9b76979ce59e5896e8ab4f405ffdd4123a536598
PhilippeMorier <morier.dev@outlook.com>
2017-08-13 15:50:52 +0100

    refactor(environment): tighten `firebase` to `FirebaseAppConfig`

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,7 @@
+import { FirebaseAppConfig } from 'angularfire2';
+
 export interface Environment {
-  firebase: {};
+  firebase: FirebaseAppConfig;
   name: 'DEVELOPMENT' | 'STAGING' | 'PRODUCTION';
   production: boolean;
 }
